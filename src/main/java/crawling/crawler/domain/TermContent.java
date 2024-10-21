ffc7601c20f5ff8865af1d8d98e16f4d5330a282
Jang Seung Beom <wkdtmf357@gmail.com>
2024-10-22 02:42:07 +0900

    feat: TermContent 클래스 추가

--- src/main/java/crawling/crawler/domain/TermContent.java
+++ src/main/java/crawling/crawler/domain/TermContent.java
@@ -1,27 +1,16 @@
 package crawling.crawler.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Lob;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Entity @Getter
-@AllArgsConstructor @NoArgsConstructor
-public class TermInfo {
-
-    @Id
-    @Column(name = "TERM_ID")
-    private Long id; // 법령용어 ID
-
+@Getter @Setter
+@AllArgsConstructor
+public class TermContent {
     private String termName; // 법령용어명
 
-    @Lob
     private String source; // 출처
 
-    @Lob
     private String description; // 법령용어정의
 
 }
